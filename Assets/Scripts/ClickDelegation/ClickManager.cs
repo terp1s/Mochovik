c745571c37ca9555b4ed5aedbf8ef44c37b72368
terp1s <piskova.ter@gmail.com>
2026-08-03 13:47:30 +0200

    intro scene

--- Assets/Scripts/ClickDelegation/ClickManager.cs
+++ Assets/Scripts/ClickDelegation/ClickManager.cs
@@ -4,31 +4,32 @@ using UnityEngine;
 
 public class ClickManager : MonoBehaviour
 {
-    void Update()
+    private void LeftMouseClicked()
     {
-        if (Input.GetMouseButtonDown(0))
+        Debug.Log("Mouse Clicked");
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+        RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+        if (hit.collider != null)
         {
-            Debug.Log("Mouse Clicked");
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            Debug.Log(hit.collider.gameObject.name);
 
-            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-
-            if (hit.collider != null)
+            if (hit.collider.TryGetComponent(out IInteractable interactable))
             {
-                Debug.Log(hit.collider.gameObject.name);
-
-                if (hit.collider.TryGetComponent(out IInteractable interactable))
-                {
-                    interactable.Interact();
-                }
-                // PRIORITY 2: If not interactable, can I walk on it? (Floor, Ground)
-                else if (hit.collider.TryGetComponent(out IWalkable walkable))
-                {
-                    walkable.OnWalkTo(hit.point); // Pass the exact hit point!
-                }
+                interactable.Interact();
             }
 
+            else if (hit.collider.TryGetComponent(out IWalkable walkable))
+            {
+                walkable.OnWalkTo(hit.point); // Pass the exact hit point!
+            }
         }
+    }
+    private void OnEnable()
+    {
+        GameEventsManager.instance.inputEvents.onLeftMouseClicked += LeftMouseClicked;
+
     }
 }
